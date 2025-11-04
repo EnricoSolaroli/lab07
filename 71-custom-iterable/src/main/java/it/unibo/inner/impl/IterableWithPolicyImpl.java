d7a24eb34d7d0fc461507063c1fbd882b9b4bfe7
Enrico Solaroli <enrico.solaroli@studio,unibo,it>
2025-11-04 16:20:42 +0100

    add resolution of part1 of exercise 71

---
+++ 71-custom-iterable/src/main/java/it/unibo/inner/impl/IterableWithPolicyImpl.java
@@ -0,0 +1,49 @@
+package it.unibo.inner.impl;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+import it.unibo.inner.api.IterableWithPolicy;
+import it.unibo.inner.api.Predicate;
+
+public class IterableWithPolicyImpl<T> implements IterableWithPolicy<T> {
+
+    final private List<T> elements;
+
+    public IterableWithPolicyImpl(T[] elements){
+        this.elements = List.of(elements); 
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new IteratorImpl();
+    }
+
+    @Override
+    public void setIterationPolicy(Predicate filter) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setIterationPolicy'");
+    }
+
+   public String toString(){
+        return this.elements.toString();
+   }
+
+    class IteratorImpl implements Iterator<T>{
+
+        private int current = 0;
+
+        @Override
+        public boolean hasNext() {
+            return IterableWithPolicyImpl.this.elements.size() > this.current;
+        }
+        @Override
+        public T next() { 
+            return IterableWithPolicyImpl.this.elements.get(current++);
+        }
+
+    }
+    
+}
