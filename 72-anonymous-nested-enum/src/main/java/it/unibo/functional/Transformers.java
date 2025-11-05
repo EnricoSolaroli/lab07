97cee8763b409461b74fd5c0fd9d16f682201e58
Enrico Solaroli <enrico.solaroli@studio,unibo,it>
2025-11-05 18:08:08 +0100

    add solution 72 excercise

--- 72-anonymous-nested-enum/src/main/java/it/unibo/functional/Transformers.java
+++ 72-anonymous-nested-enum/src/main/java/it/unibo/functional/Transformers.java
@@ -28,7 +28,7 @@ public final class Transformers {
      * @param <O> output elements type
      * @return A "flattened" list of the produced elements
      */
-    public static <I, O> List<O> flattenTransform(
+    public static <I, O> List<O> flattenTransform (
         final Iterable<? extends I> base,
         final Function<I, ? extends Collection<? extends O>> transformer
     ) {
@@ -53,12 +53,14 @@ public final class Transformers {
      * @param <O> output elements type
      * @return A transformed list where each input element is replaced with the produced elements
      */
-    public static <I, O> List<O> transform(final Iterable<I> base, final Function<I, O> transformer) {
-        return flattenTransform(base, new Function<I, List<O>>() {
-            public List<O> call(I input){
-                O result = transformer.call(input);
-                return List.of(result);
-            }
+    public static <I, O> List<O> transform (
+        final Iterable<I> base,
+        final Function<I, O> transformer
+    ) {
+        return flattenTransform(base, new Function<I, Collection<O>>() {
+            public Collection<O> call(I input){
+                return List.of(transformer.call(input));
+            } 
         });
     }
 
@@ -75,7 +77,7 @@ public final class Transformers {
      * @return A flattened list with the elements of each collection in the input
      */
     public static <I> List<? extends I> flatten(final Iterable<? extends Collection<? extends I>> base) {
-        return null;
+        return flattenTransform(base, Function.identity());
     }
 
     /**
@@ -92,7 +94,11 @@ public final class Transformers {
      * @return A list containing only the elements that passed the test
      */
     public static <I> List<I> select(final Iterable<I> base, final Function<I, Boolean> test) {
-        return null;
+        return flattenTransform(base, new Function<I, Collection<I>>() {
+            public Collection<I> call(I input){
+                return (test.call(input)) ? List.of(input) : List.of();
+            } 
+        });
     }
 
     /**
@@ -108,6 +114,10 @@ public final class Transformers {
      * @return A list containing only the elements that passed the test
      */
     public static <I> List<I> reject(final Iterable<I> base, final Function<I, Boolean> test) {
-        return null;
+        return select(base, new Function<I, Boolean>() {
+            public Boolean call(I input) {
+                return ! test.call(input);
+            }            
+        });
     }
 }
